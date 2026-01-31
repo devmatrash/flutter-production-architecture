e35324337684738a79cfa2de1473842d6efa84eb
Mahmoud Alatrash <devmatrash@gmail.com>
2026-01-31 12:13:37 +0200

    Refactor app entry and add HandbookApp widget
    Introduced a new HandbookApp widget in its own file and refactored main.dart to use runZonedGuarded for error handling. Removed the default MyApp and MyHomePage widgets, simplifying the app's entry point.

---
+++ lib/handbook_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HandbookApp extends StatelessWidget {
+  const HandbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: SafeArea(child: Container())),
+    );
+  }
+}
